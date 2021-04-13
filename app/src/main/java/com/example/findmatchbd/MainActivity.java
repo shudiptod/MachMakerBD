60676013d164986ce1c91ce07a570c22b4641764
shudipto <shudipta15-11424@diu.edu.bd>
2021-04-13 17:26:59 +0600

    Added: Connecting by Right Swipe Disconnecting By Left Swipe

--- app/src/main/java/com/example/findmatchbd/MainActivity.java
+++ app/src/main/java/com/example/findmatchbd/MainActivity.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -22,26 +23,39 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.lorentzos.flingswipe.SwipeFlingAdapterView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ArrayList<String> al;
-    private ArrayAdapter<String> arrayAdapter;
+    private cards cards_data[];
+
+    private arrayAdapter arrayAdapter;
     private int i;
 
     private FirebaseAuth mAuth;
 
+    private String currentUId;
+    private DatabaseReference usersDb;
+
+
+    ListView listView;
+    List<cards> rowItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        usersDb = FirebaseDatabase.getInstance().getReference().child("Users");
+
         mAuth=FirebaseAuth.getInstance();
+        currentUId = mAuth.getCurrentUser().getUid();
         checkUserGender();
 
-        al = new ArrayList<>();
+        rowItems = new ArrayList<cards>();
 
-        arrayAdapter = new ArrayAdapter<>(this, R.layout.item, R.id.helloText, al );
+        arrayAdapter = new arrayAdapter(this, R.layout.item, rowItems );
 
         SwipeFlingAdapterView flingContainer = findViewById(R.id.frame);
         flingContainer.setAdapter(arrayAdapter);
@@ -50,20 +64,27 @@ public class MainActivity extends AppCompatActivity {
             public void removeFirstObjectInAdapter() {
                 // this is the simplest way to delete an object from the Adapter (/AdapterView)
                 Log.d("LIST", "removed object!");
-                al.remove(0);
+                rowItems.remove(0);
                 arrayAdapter.notifyDataSetChanged();
             }
 
             @Override
             public void onLeftCardExit(Object dataObject) {
-                //Do something on the left!
-                //You also have access to the original object.
-                //If you want to use it just cast it (String) dataObject
+
+                cards obj = (cards) dataObject;
+                String userId = obj.getUserId();
+                usersDb.child(userOppositeGender).child(userId).child("connections").child("no").child(currentUId).setValue(true);
                 Toast.makeText(MainActivity.this, "Left",Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onRightCardExit(Object dataObject) {
+
+
+                cards obj = (cards) dataObject;
+                String userId = obj.getUserId();
+                usersDb.child(userOppositeGender).child(userId).child("connections").child("yes").child(currentUId).setValue(true);
+
                 Toast.makeText(MainActivity.this, "Right",Toast.LENGTH_SHORT).show();
             }
 
@@ -169,8 +190,10 @@ public class MainActivity extends AppCompatActivity {
         oppositeUserDb.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-                if (snapshot.exists()){
-                    al.add(snapshot.child("name").getValue().toString());
+                if (snapshot.exists() && !snapshot.child("connections").child("no").hasChild(currentUId) && !snapshot.child("connections").child("yes").hasChild(currentUId)){
+
+                    cards item = new cards(snapshot.getKey(),snapshot.child("name").getValue().toString());
+                    rowItems.add(item);
                     arrayAdapter.notifyDataSetChanged();
                 }
             }
---
+++ app/src/main/java/com/example/findmatchbd/cards.java
@@ -0,0 +1,26 @@
+package com.example.findmatchbd;
+
+public class cards {
+
+    private String userId;
+    private String name;
+
+    public cards(String userId, String name){
+        this.userId = userId;
+        this.name = name;
+    }
+
+    public String getUserId(){
+        return userId;
+    }
+    public void setUserId(String userId){
+        this.userId = userId;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+}
